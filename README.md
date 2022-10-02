a5d760853d4c47964d81ae37d6465aaa8e26c3f8
joaquinidlt <joaquindlt16@gmail.com>
2022-10-02 14:30:20 +0200

    css and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Days-to-the-event
+
+
+## App
+
+With this app, you will be able to see the days left until the event you have added and the information will not be lost when you refresh the screen or close and reopen the application. 
+
+
+
+
+!<p align="center">
+  <img width="500" height="350" src="./img/screenDaysToTheEvent.png" />
+</p>
--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const eventDate = document.querySelector('#eventDate'); // saco la referencia "e
 const buttonAdd = document.querySelector('#bAdd'); // saco la referencia "bAdd"
 const eventContainer = document.querySelector('#eventsContainer'); // saco la referencia "eventsContainer"
 
+const json = load(); // guardo lo que tengo almacenado en "load()" en "json"
+// Cada vez que entro a mi aplicacion voy a intentar de recuperar la informacion con la funcion "load()",
+// y voy a tratar de parcerizarla "arr = JSON.parse(json);"
+
+try {
+    arr = JSON.parse(json); // guardo en mi arreglo json con formato JSON
+} catch (error) {
+    arr = [];
+}
+events = arr ? [...arr] : []; // si no pasa por catch, evaluamos de que si "arr" existe (diferente de null or undefine), quiero que copie lo que tengo en ese "arr" para "events".
+
+renderEvents();
+
 document.querySelector('form').addEventListener('submit', (e) => { // saco la referencia a mi "form" y luego con "addEventListener" le dijo que cuando haga click em submit, ejecute la siguiente funcion.
     e.preventDefault();
     addEvent();
@@ -29,6 +42,8 @@ function addEvent() {
 
     events.unshift(newEvent);
 
+    save(JSON.stringify(events)); // llamo mi funcion "save()" y llamo mi objeto JSON global y convierto en string my arreglo de eventos "JSON.stringify(events)"
+
     eventName.value = "";
 
     renderEvents();
@@ -48,7 +63,7 @@ function renderEvents() {
             <div class="event">
                 <div class="days">
                     <span class="days-number">${dateDiff(event.date)}</span>
-                    <span class="days-text">days</span>
+                    <span class="days-text">days to</span>
                 </div>
 
                 <div class="event-name">${event.name}</div>
@@ -66,7 +81,17 @@ function renderEvents() {
             const id = button.getAttribute('data-id');
             events = events.filter((event) => event.id !== id);
 
+            save(JSON.stringify(events));
+
             renderEvents();
         });
     });
+}
+
+function save(data) {
+    localStorage.setItem('items', data);// usamos el objeto de almacenaiento "localStorage", usamos el metodo de almacenamiento "setItem", le damos un nombre a lo que vamos a guardar "items" y despues le pasamos el valor "data".
+}
+
+function load() {
+    return localStorage.getItem('items'); // con esta funcion regresamos la informacion.
 }
